077210536b4a683f801c6df14379c8a8521b0af3
eneunaber <eric@smips.com>
2011-02-10 21:28:12 -0600

    changed how methods where being added to SlideShows prototype object

--- simpleslideshow/slideshow.js
+++ simpleslideshow/slideshow.js
@@ -1,65 +1,60 @@
-﻿/*
-* SimpleSlideshow - A JavaScript slideshow application based on jQuery
-*
-* http://XXXXX
-*
-* Copyright (c) 2011 Eric Neunaber
-* Licensed under the MIT (MIT.License.txt).
+﻿function SimpleSlideShow() {
+    this._ssName = "ul.slideShow";
+    this._liName = this._ssName + "> li";
+    this._duration = 1000;
+    this._imagePos = 0;
+}
+
+SimpleSlideShow.prototype = {
+    isReady: function () {
+        return jQuery(this._ssName).length == 1;
+    },
+
+    imageCount: function () {
+        if (this.isReady()) {
+            return jQuery(this._liName).length;
+        }
+        return false;
+    },
+
+    setDuration: function (duration) {
+        if (duration >= 0) {
+            this._duration = duration;
+        }
+    },
+
+    begin: function () {
+        if (this.isReady()) {
+            var images = jQuery(this._liName);
+
+            jQuery(images[0]).css("display", "list-item");
+
+            for (var count = 1; count < (images.length); count++) {
+                jQuery(images[count]).css("display", "none");
+            }
+
+            setInterval(this.switchImage, 5000);
+        }
+    },
+
+    switchImage: function () {
+        var _self = this;
+        if (_self.isReady.call(_self)) {
+            var images = jQuery(_self._liName);
+            jQuery(images[_self._imagePos]).fadeOut(_self._duration);
+            _self._imagePos = ((_self._imagePos + 1) == _self.imageCount.call(_self)) ? 0 : _self._imagePos + 1;
+            jQuery(images[_self._imagePos]).fadeIn(_self._duration);
+        }
+    }
+
+};
+
+/*
+
+jQuery(function () {
+var slideShow = new SimpleSlideShow();
+slideShow.begin();
+});
+
 */
 
-(function (jQuery, win) {
-    function SlideShow() {
-        var _ssName = "ul.slideShow";
-        var _liName = _ssName + "> li";
-        var _duration = 1000;
-        var _imagePos = 0;
-        var _self = this;
-
-        this.isReady = function () {
-            return $(_ssName).length == 1;
-        };
-
-        this.imageCount = function () {
-            if (this.isReady()) {
-                return $(_liName).length;
-            }
-            return false;
-        };
-
-        this.setDuration = function (duration) {
-            if (duration >= 0) {
-                _duration = duration;
-            }
-        };
-
-        this.begin = function () {
-            if (this.isReady()) {
-                var images = $(_liName);
-
-                $(images[0]).css("display", "list-item");
-
-                for (var count = 1; count < (images.length); count++) {
-                    $(images[count]).css("display", "none");
-                }
-
-                setInterval(this.switchImage, 5000);
-            }
-        };
-
-        this.switchImage = function () {
-            if (_self.isReady()) {
-                var images = $(_liName);
-                $(images[_imagePos]).fadeOut(_duration);
-                _imagePos = ((_imagePos + 1) == _self.imageCount()) ? 0 : _imagePos + 1;
-                $(images[_imagePos]).fadeIn(_duration);
-            }
-        };
-
-    };
-
-
-    win.SimpleSlideShow = new SlideShow();
-    win.ResetSimpleSlideShow = function () {
-        win.SimpleSlideShow = new SlideShow();
-    }
-})(jQuery, window);
